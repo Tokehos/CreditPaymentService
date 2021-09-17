c4a3aaf644375bf7d8922dfd8779a24819f26e3d
Tokehos <88163971+Tokehos@users.noreply.github.com>
2021-09-17 08:32:14 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,19 @@ public class Main {
         double credit12 = 1_000_000;
         double time12 = 12;
         double monthlypayment12 = service.calculate(credit12, time12, interestRate12);
-        System.out.println(monthlypayment12);
+        System.out.println("Ежемесяный платеж: " +  monthlypayment12);
 
 
         double interestRate24 = 9.99;
         double credit24 = 1_000_000;
         double time24 = 24;
         double monthlypayment24 = service.calculate(credit24, time24, interestRate24);
-        System.out.println(monthlypayment24);
+        System.out.println("Ежемесяный платеж: " +  monthlypayment24);
 
         double interestRate36 = 9.99;
         double credit36 = 1_000_000;
         double time36 = 36;
         double monthlypayment36 = service.calculate(credit36, time36, interestRate36);
-        System.out.println(monthlypayment36);
+        System.out.println("Ежемесяный платеж: " +  monthlypayment36);
     }
 }
